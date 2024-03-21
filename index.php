6be403930b0a7d0ae25dfd1f7f3038eed28af62f
Saka <76771393+antsaka@users.noreply.github.com>
2024-03-21 22:14:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <!-- Atur pengkodean karakter -->
+    <meta charset="UTF-8">
+    <!-- Pastikan rendering yang tepat dan zoom layar sentuh -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tetapkan pengaturan viewport untuk responsivitas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Tautkan stylesheet eksternal -->
+    <link rel="stylesheet" href="style.css" />
+    <!-- Preconnect ke sumber font eksternal -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Tautkan font kustom -->
+    <link href="https://fonts.googleapis.com/css2?family=Madimi+One&display=swap" rel="stylesheet">
+    <!-- Tetapkan judul halaman -->
+    <title>Linktree</title>
+  </head>
+  <body>
+    <div>
+      <!-- Bagian Profil -->
+      <div class="profile">
+        <!-- Tampilkan gambar profil -->
+        <img src="profile.jpg" alt="error image" />
+        <!-- Tampilkan nama -->
+        <h1>Juanita Max Wynn</h1>
+        <!-- Tampilkan deskripsi -->
+        <p>Influencer Balik Papan</p>
+      </div>
+
+      <!-- Bagian Linktree -->
+      <div class="link">
+        <!-- Tautan ke Instagram -->
+        <a href="https://instagram.com/dea.afrizal" target="_blank">instagram</a>
+        <!-- Tautan ke Telegram -->
+        <a href="https://telegram.com" target="_blank" >telegram</a>
+        <!-- Tautan ke Discord -->
+        <a href="https://discord.com" target="_blank" >discord</a>
+        <!-- Tautan ke YouTube -->
+        <a href="https://youtube.com" target="_blank">youtube</a>
+        <!-- Contoh video YouTube yang disematkan -->
+        <!-- <iframe width="360" height="115" src="https://www.youtube.com/embed/-di1X-kuFNc?si=RgaTQpYTQEZ2RH5j" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> -->
+        <!-- Embed Spotify -->
+        
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/60a4bOysufBKLU5Rq6DsoK?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      </div>
+
+      <!-- Bagian Footer -->
+      <div>
+        <!-- Tampilkan pesan -->
+        <i>dibuat dengan cinta muach</i>
+      </div>
+    </div>
+  </body>
+</html>
